c2d5eeefccde65bdc4cbdf1f8a5d388a7f2c2bb1
Rohan Chandra <rc8yw@virginia.edu>
2020-01-19 17:10:31 -0500

    First commit.

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const foodAssistRoutes = express.Router();
+const PORT = 4000;
+
+
